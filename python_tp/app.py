b2a8ac3270c31e9d28609ded6c415d622c478cba
ctfree <ctfree@users.noreply.github.com>
2022-09-17 03:21:30 +0000

    ✅ Ready to clone and code.

--- python_tp/app.py
+++ python_tp/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+python_tp API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="python_tp",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://python_tp.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "ctfree",
+        "url": "http://python_tp.com/contact/",
+        "email": "ctfree@python_tp.com",
     },
     license_info={
         "name": "The Unlicense",
